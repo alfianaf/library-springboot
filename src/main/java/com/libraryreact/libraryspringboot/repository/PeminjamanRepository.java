ed239072a0f06b07e9d02ee36f94823625809ad2
Alfian Aulia Firdaus <alfian@kompiscreative.tech>
2021-04-23 16:19:11 +0700

    pengembalian buku not tested yet

--- src/main/java/com/libraryreact/libraryspringboot/repository/PeminjamanRepository.java
+++ src/main/java/com/libraryreact/libraryspringboot/repository/PeminjamanRepository.java
@@ -1,12 +1,17 @@
 package com.libraryreact.libraryspringboot.repository;
 
+import java.util.List;
+
 import com.libraryreact.libraryspringboot.models.entity.Peminjaman;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PeminjamanRepository extends JpaRepository<Peminjaman, Integer> {
+    public List<Peminjaman> findByIsFinished(Boolean isFinished);
 
+    @Query(value = "select * from peminjaman where id = ?", nativeQuery = true)
+    public Peminjaman findDetailById(Integer userId);
 }
--- src/main/java/com/libraryreact/libraryspringboot/service/PeminjamanService.java
+++ src/main/java/com/libraryreact/libraryspringboot/service/PeminjamanService.java
@@ -3,6 +3,15 @@ package com.libraryreact.libraryspringboot.service;
 import com.libraryreact.libraryspringboot.models.dto.PeminjamanDto;
 import com.libraryreact.libraryspringboot.models.dto.StatusMessageDto;
 
+import org.springframework.http.ResponseEntity;
+
 public interface PeminjamanService {
     public StatusMessageDto<PeminjamanDto> sewaBuku(PeminjamanDto dto);
+
+    public ResponseEntity<?> pengembalianBuku(PeminjamanDto peminjamanDto);
+
+    public ResponseEntity<?> getAll();
+
+    public ResponseEntity<?> getDetailById(Integer id);
+
 }
